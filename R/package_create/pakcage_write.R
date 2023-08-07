a73cb22ca69ca886f0e9d69aacc52264f3963406
John Gavin <john.b.gavin@gmail.com>
2023-08-07 11:43:11 +0100

    usethis and credentials to create/set GITHUB_PAT on github

--- R/package_create/pakcage_write.R
+++ R/package_create/pakcage_write.R
@@ -31,8 +31,20 @@ library(wordparse)
 use_test("extract_vowels")
 test(my_folder)
 
+# GITHUB_PAT
+# usethis::edit_r_environ()
+# Chp 10: Happy Git With R - setup a new PAT
+usethis::create_github_token()
+library(credentials)
+set_github_pat()
+# change authentication - paste in your PAT
+gitcreds::gitcreds_set()
+gh::gh_whoami()
+# make sure everything is looking good
+usethis::git_sitrep()
+
+# https://stackoverflow.com/questions/22565024/how-to-properly-include-dependencies-in-r-package
 
 # x <- '/Users/johngavin/Documents_GitHub/wordparse/renv/library/R-4.3/aarch64-apple-darwin20/wordparse/R/wordparse.rdb'
 # file.remove(x)
 
-# https://stackoverflow.com/questions/22565024/how-to-properly-include-dependencies-in-r-package
