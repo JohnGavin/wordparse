67da3f70ac359e8b76efd8c8644cd51d36a37c5c
John Gavin <john.b.gavin@gmail.com>
2023-08-08 09:56:53 +0100

    snapshot type = explicit so read deps from DESCRIPTION file _only_

---
+++ R/package_create/renv.R
@@ -0,0 +1,36 @@
+
+# https://cloud.r-project.org/web/packages/renv/vignettes/faq.html
+
+rstudioapi::restartSession()
+# only include packages which are explicitly listed in the project’s
+# DESCRIPTION file as dependencies
+
+file.remove(".Rprofile", "renv.lock")
+unlink("renv", recursive = TRUE)
+rstudioapi::restartSession()
+# renv::settings$snapshot.type("explicit")
+options(renv.settings.snapshot.type = "explicit")
+renv::init()
+rstudioapi::restartSession()
+renv::status()
+# OR
+# restore project state as defined in the lockfile
+renv::restore()
+
+renv::clean()
+renv::purge("devtools")
+renv::repair()
+renv::rebuild()
+
+# Verify code works then renv::snapshot() to update new lockfile
+renv::snapshot() # to update the lockfile
+
+# may not capture the packages that you use in
+# iterative workflows (e.g. devtools
+# DESCRIPTION file; typically, within the Suggests: field.
+# Then execute (install the packages as described in the DESCRIPTION)
+options(renv.settings.snapshot.type = "explicit")
+renv::install()
+
+# renv can see into subfolders!
+renv::dependencies()
